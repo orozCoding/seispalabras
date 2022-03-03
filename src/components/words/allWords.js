7ccf2b76c605b60fa5e505a094d5b6a440a40f94
orozcoding <orozcoding@gmail.com>
2022-03-03 01:32:23 -0400

    Add more words

--- src/components/words/allWords.js
+++ src/components/words/allWords.js
@@ -1,23 +1,37 @@
 const words = [{
   id: 1,
-  e: "car",
-  s: ["auto", "carro", "coche"],
-  completed: "false",
-  active: "false",
+  e: 'car',
+  s: ['auto', 'carro', 'coche'],
+  completed: 'false',
+  active: 'false',
 },
 {
   id: 2,
-  e: "phone",
-  s: ["móvil", "celular", "teléfono"],
-  completed: "false",
-  active: "false",
+  e: 'phone',
+  s: ['móvil', 'celular', 'teléfono'],
+  completed: 'false',
+  active: 'false',
 },
 {
   id: 3,
-  e: "computer",
-  s: ["computadora", "computador", "ordenador"],
-  completed: "false",
-  active: "false",
-}]
+  e: 'computer',
+  s: ['computadora', 'computador', 'ordenador'],
+  completed: 'false',
+  active: 'false',
+},
+{
+  id: 4,
+  e: 'shoe',
+  s: ['zapato', 'calzado', 'zapatilla', 'botín', 'bota'],
+  completed: 'false',
+  active: 'false',
+},
+{
+  id: 5,
+  e: 'friend',
+  s: ['amigo', 'amiga'],
+  completed: 'false',
+  active: 'false',
+}];
 
 export default words;
