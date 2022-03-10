133890e862ef47780c94788f19916082cfcbd678
orozcoding <orozcoding@gmail.com>
2022-03-10 01:12:20 -0400

    Add number to completed items

--- src/pages/completed.js
+++ src/pages/completed.js
@@ -2,20 +2,22 @@ import React from "react";
 
 const Completed = (props) => {
   const { completed } = props;
-
+  let i = 0;
   return (
     <section className="completed-container d-flex col">
       <p className="title bold">Completed Words</p>
       {completed.length > 0 && (<p>Check the words you have translated:</p>)}
       {!completed.length && (<p>After translating the first word, the completed ones will be stored here.</p>)}
       <div className="completedWords d-flex col">{completed.map((word) => {
+        i += 1;
         return (
           <div key={word.id} className="d-flex col">
-            <p><span className="completedWord">{word.e.toUpperCase()}</span> in Spanish can be:</p>
+            <p>{i}. <span className="completedWord">{word.e.toUpperCase()}</span> in Spanish can be:</p>
             {word.s.length > 1 && (<p><span className="possibleWord">{word.s.join(', ')}</span> and more.</p>)}
             {word.s.length < 2 && (<p><span className="possibleWord">{word.s.join(', ')}</span>.</p>)}
           </div>
         )
+        
       })}
       <p className="finalMsg">Comeback tomorrow and play with more words.</p></div>
     </section>
