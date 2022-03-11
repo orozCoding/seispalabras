af8e5957b03ca08ddeea54a3ab69e6c457676134
orozcoding <orozcoding@gmail.com>
2022-03-11 04:54:54 -0400

    Fix date check

--- src/components/dates/dates.js
+++ src/components/dates/dates.js
@@ -1,13 +1,13 @@
 import { DateTime } from "luxon";
 
-const date = DateTime.now().toLocaleString();
+const date = DateTime.now().toObject();
 
 const getLastVisit = () => {
-  return JSON.parse(localStorage.getItem('lastVisit'));
+  return JSON.parse(localStorage.getItem('lastCheck'));
 }
 
 const storageDate = () => {
-  localStorage.setItem('lastVisit', JSON.stringify(date));
+  localStorage.setItem('lastCheck', JSON.stringify(date));
 }
 
 const checkSameDay = () => {
@@ -18,21 +18,27 @@ const checkSameDay = () => {
 };
 
 const checkNewDay = () => {
-  if (date > getLastVisit()) {
+  if(!getLastVisit()){
+    return true;
+  }
+  
+  if ((date.year > getLastVisit().year)
+    || (date.month > getLastVisit().month && date.year >= getLastVisit().year)
+    || (date.day > getLastVisit().day && date.month >= getLastVisit().month && date.year >= getLastVisit().year)) {
     return true;
   }
   return false;
 };
 
 const storageNewDay = () => {
-  if(!getLastVisit() || checkNewDay()){
+  if (!getLastVisit() || checkNewDay()) {
     storageDate();
   }
 }
 
-export { 
+export {
   storageDate,
   checkSameDay,
   checkNewDay,
   storageNewDay
- };
+};
